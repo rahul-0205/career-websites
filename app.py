8a0d9e8037f360b70684911a44b8a86d8e9f82a6
rahul-0205 <125853861+rahul-0205@users.noreply.github.com>
2023-03-30 10:29:32 +0000

    add job item

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 from flask import  Flask
 from flask import render_template
 app=Flask(__name__)
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Delhi,India',
+    'salary':'Rs. 11,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Bengaluru, India',
+    'salary':'Rs. 7,00,000'
+  },
+  {
+    'id':3,
+    'title':'Software Engineer',
+    'location':'Chennai,India',
+    'salary':'Rs. 12,00,000'
+  },
+  {
+    'id':4,
+    'title':'Frontend developer',
+    'location':'Remote',
+    'salary':'Rs. 1,00,000'
+  }
+]
 @app.route("/")
 def hello():
-  return render_template('index.html')
+  return render_template('index.html',jobs=JOBS)
 if __name__ =='__main__':
   app.run(host='0.0.0.0', debug=True)
